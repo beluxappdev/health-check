6de2c6ddeb5388efffccb2e687d838ee65651eb5
Gianluigi Liguori <gianluigiliguori@live.it>
2023-12-01 14:41:13 +0100

    Artifacts publishing

--- README.md
+++ README.md
@@ -1,33 +1,67 @@
-# Project
+# AKS Well-Architected Assessment
 
-> This repo has been populated by an initial template to help get you started. Please
-> make sure to update the content to build a great experience for community-building.
+## Project Introduction:
+Kubernetes and its Azure implementation has become a market standard for deploying modern cloud-oriented microservice applications. 
+The purpose of the assessment is to verify that Microsoft good practices are applied to prevent security or reliability problems from compromising compliance with the service level agreements guaranteed by the hosted applications in the future.
 
-As the maintainer of this project, please make a few updates:
 
-- Improving this README.MD file to provide a great experience
-- Updating SUPPORT.MD with content about this project's support experience
-- Understanding the security reporting process in SECURITY.MD
-- Remove this section from the README
+## Checks implemented
 
-## Contributing
+### Security:
+- Cluster must be private
+- NSG should be linked to node pools subnets
+- External IP addresses should not be associated with AKS load balancer 
+- Network policies (Azure or Calico) should be enabled
+- Public FQDN should be disabled
+- Defender profile should be enabled
+- Policy add-on should be enabled
+- KMS should be enabled
+- RBAC should be enabled
+- Microsoft Entra ID authentication should be enabled
+- Workload Identity should be enabled
+- AAD PodIdentity should not be used
+- Leverage Cluster Managed Identity
+- Disable local accounts
 
-This project welcomes contributions and suggestions.  Most contributions require you to agree to a
-Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
-the rights to use your contribution. For details, visit https://cla.opensource.microsoft.com.
+### Observability:
+- Container Insights should be enabled
+- Diagnostic Settings should be enabled
+  
+### Resiliency:
+- Availability zones should be used
+- Uptime SLA should be enabled
+- Control plane and node pools version should not be outdated
+- Cluster should have at least one system and one user node pool
 
-When you submit a pull request, a CLA bot will automatically determine whether you need to provide
-a CLA and decorate the PR appropriately (e.g., status check, comment). Simply follow the instructions
-provided by the bot. You will only need to do this once across all repos using our CLA.
+### Networking
+- Azure Overlay network should be used
 
-This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
-For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
-contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+### Performance
+- Leverage Ephemeral OS Disk
 
-## Trademarks
+## Getting started
 
-This project may contain trademarks or logos for projects, products, or services. Authorized use of Microsoft 
-trademarks or logos is subject to and must follow 
-[Microsoft's Trademark & Brand Guidelines](https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/usage/general).
-Use of Microsoft trademarks or logos in modified versions of this project must not cause confusion or imply Microsoft sponsorship.
-Any use of third-party trademarks or logos are subject to those third-party's policies.
+### Prerequisites
+- Windows PowerShell 5+ / PowerShell Core 7+
+- Azure CLI 
+- "account" extension installed on Azure CLI: `az extension add --name account`
+
+### Permissions
+In order to complete the assessment, the user must have at least Reader permission/role over the Azure Kubernetes Service resources
+
+### Execution
+Login to the Azure CLI and execute the script
+
+```powershell
+az login # -t [tenant id] (optional)
+
+# Optionally specify the output path and the CSV Delimiter as argument
+.\StartAKSClusterAssessment.ps1 #-Path "C:\MyPath" -csvDelimiter ","
+
+```
+
+### Output 
+An output folder named "AKSClusterAssessment_YYYY-MM-DD_hh-mm-ss" will be created for each assessment execution. The folder will contain the following files:
+
+- Assessment_Result_YYYY-MM-DD_hh-mm-ss - CSV Results of the assessment
+- Assessment_Log_YYYY-MM-DD_hh-mm-ss - Transcript of the PowerShell execution
---
+++ StartAKSClusterAssessment.ps1
@@ -0,0 +1,290 @@
+param (
+    [Parameter(Mandatory = $false)][string]$Path = $PSScriptRoot,
+    [Parameter(Mandatory = $false)][string]$csvDelimiter = ";"
+)
+
+$okMessage = "OK";
+$koMessage = "KO";
+
+class AKSClusterResult {
+    [string]$Compliant
+    [string]$SubscriptionId
+    [string]$SubscriptionName
+    [string]$ResourceGroup
+    [string]$ClusterName
+    [string]$ProvisioningState
+    [string]$Region
+    [string]$Version
+    [string]$ManagedResourceGroup
+    [string]$PrivateCluster
+    [string]$PrivateClusterPublicFqdn
+    [string]$LoadBalancerWithoutPublicIp
+    [string]$NodePoolSubnetWithNSG
+    [string]$NetworkPolicyAzureCalico
+    [string]$NetworkPluginMode
+    [string]$IsKubernetesVersionSupported
+    [string]$ContainerInsights
+    [string]$DiagnosticSettings
+    [string]$UserAssignedIdentity
+    [string]$PodIdentityDeprecated
+    [string]$MicrosoftDefender
+    [string]$RBAC
+    [string]$AzureADIntegration
+    [string]$DisableLocalAccounts
+    [string]$KMSConfigured
+    [string]$AzurePolicy
+    [string]$WorkloadIdentity
+    [string]$SystemAndUserNodePool
+    [string]$AvailabilityZones
+    [string]$UptimeSlaConfiguration
+    [string]$CurrentNodepoolCount
+    [string]$CurrentTotalNodeCount
+    [string]$OsDiskType
+    [string]$ErrorMessage
+}
+
+function Start-AKSClusterAssessment {
+    $subscriptions = az account subscription list -o json | ConvertFrom-Json
+
+    foreach ($currentSubscription in $subscriptions) {
+          
+        Write-Host "***** Assessing the subscription $($currentSubscription.displayName) ($($currentSubscription.id)..." -ForegroundColor Green
+        az account set -s $currentSubscription.SubscriptionId
+
+        $aksClusters = az aks list -o json | ConvertFrom-Json
+        foreach ($currentAKSCluster in $aksClusters) {
+            Write-Host "**** Assessing the AKS Cluster $($currentAKSCluster.Name)..." -ForegroundColor Blue
+
+            
+
+            $aksResult = [AKSClusterResult]::new()
+
+            try {
+                $aksResult.SubscriptionId = $currentSubscription.SubscriptionId
+                $aksResult.SubscriptionName = $currentSubscription.displayName
+                $aksResult.ResourceGroup = $currentAKSCluster.resourceGroup
+                $aksResult.ClusterName = $currentAKSCluster.Name
+                $aksResult.ProvisioningState = $currentAKSCluster.provisioningState
+                $aksResult.Region = $currentAKSCluster.location
+                $aksResult.Version = $currentAKSCluster.kubernetesVersion
+                $aksResult.ManagedResourceGroup = $currentAKSCluster.nodeResourceGroup
+                $aksResult.CurrentNodepoolCount = $currentAKSCluster.agentPoolProfiles.Length
+                $aksResult.CurrentTotalNodeCount = GetTotalNodeCount($currentAKSCluster)
+                
+                #Private cluster should be enabled and public FQDN disabled
+                $aksResult.PrivateCluster = $(if ([string]::IsNullOrEmpty($currentAKSCluster.apiServerAccessProfile.enablePrivateCluster)) { $koMessage } else { $okMessage })
+                $aksResult.PrivateClusterPublicFqdn = $(if ([string]::IsNullOrEmpty($currentAKSCluster.privateFqdn)) { $koMessage } else { $okMessage })
+                $aksResult.LoadBalancerWithoutPublicIp = if ($currentAKSCluster.networkProfile.loadBalancerProfile.effectiveOutboundIPs.count -gt 0) { $koMessage } else { $okMessage }
+                $aksResult.NodePoolSubnetWithNSG = CheckNodePoolSubnetsHasNSG($currentAKSCluster.agentPoolProfiles)
+                $aksResult.NetworkPolicyAzureCalico = if (($currentAKSCluster.networkProfile.networkPolicy -eq "azure") -or ($currentAKSCluster.networkProfile.networkPolicy -eq "calico")) { $okMessage } else { $koMessage }
+
+                #CNI Overlay networking should be used
+                $aksResult.NetworkPluginMode = $(if ($currentAKSCluster.networkProfile.networkPluginMode -eq "overlay") { $okMessage } else { $koMessage })
+
+                #RBAC should be enabled and azure ad used for authentication, cluster identity should be managed identity 
+                $aksResult.RBAC = $(if ($currentAKSCluster.enableRbac) { $okMessage } else { $koMessage })
+                $aksResult.AzureADIntegration = $(if ([string]::IsNullOrEmpty($currentAKSCluster.aadProfile)) { $koMessage } else { $okMessage })
+                $aksResult.DisableLocalAccounts = $(if ($currentAKSCluster.disableLocalAccounts) { $okMessage } else { $koMessage })
+                $aksResult.UserAssignedIdentity = $(if ($currentAKSCluster.identity.type -eq "UserAssigned" -or $currentAKSCluster.identity.type -eq "SystemAssigned") { $okMessage } else { $koMessage })
+                $aksResult.PodIdentityDeprecated = $(if ([string]::IsNullOrEmpty($currentAKSCluster.podIdentityProfile)) { $okMessage } else { $koMessage })
+
+                #Observability, Compliance and security features
+                $aksResult.ContainerInsights = $(if ($currentAKSCluster.addonProfiles.omsagent.enabled) { $okMessage } else { $koMessage }) 
+                $aksResult.DiagnosticSettings = CheckDiagnosticSettings($currentAKSCluster)
+                $aksResult.MicrosoftDefender = $(if ([string]::IsNullOrEmpty($currentAKSCluster.securityProfile.defender)) { $koMessage } else { $okMessage })
+                $aksResult.KMSConfigured = $(if ([string]::IsNullOrEmpty($currentAKSCluster.securityProfile.azureKeyVaultKms)) { $koMessage } else { $okMessage })
+                $aksResult.AzurePolicy = $(if ($currentAKSCluster.addonProfiles.azurepolicy.Enabled) { $okMessage } else { $koMessage })
+                $aksResult.WorkloadIdentity = $(if ($currentAKSCluster.securityProfile.workloadIdentity.enabled) { $okMessage } else { $koMessage })
+
+                #High availability (control plane, node pools, uptime sla)                
+                $aksResult.AvailabilityZones = CheckAvailabilityZones($currentAKSCluster.agentPoolProfiles)
+                $aksResult.SystemAndUserNodePool = CheckNodepoolsSpecification($currentAKSCluster.agentPoolProfiles)
+                $aksResult.UptimeSlaConfiguration = $(if ( $currentAKSCluster.sku.tier -eq "Free") { $koMessage } else { $okMessage })
+                $aksResult.OsDiskType = CheckNodePoolOSDisksAreEphemeral($currentAKSCluster.agentPoolProfiles)
+                                
+                #Cluster and nodepool version should not lag x versions behind the default
+                $aksResult.IsKubernetesVersionSupported = CheckKubernetesControlPlaneVersion($currentAKSCluster)
+
+                #Obsolete features
+                #-> PodIdentity
+
+                #Other checks
+                
+                $aksResult.Compliant = Get-Compliancy $aksResult
+            }
+            catch {
+                $aksResult.ErrorMessage = $_.Exception.Message
+            }
+            Export-AKSClusterResult $aksResult
+        }
+    }
+}
+
+function CheckNodePoolSubnetsHasNSG {    
+    Param(
+        [Parameter(Mandatory = $true)]$agentPoolProfiles
+    )   
+
+    foreach ($agentProfile in $agentPoolProfiles) {
+        if ($agentProfile.vnetSubnetId) {
+            $subnet = az network vnet subnet show --ids $agentProfile.vnetSubnetId | ConvertFrom-Json
+            if (-not $subnet.networkSecurityGroup) {
+                return $koMessage
+            }
+        }
+        else {
+            return $koMessage
+        } 
+    }
+    return $okMessage;
+}
+
+function CheckNodePoolOSDisksAreEphemeral {    
+    Param(
+        [Parameter(Mandatory = $true)]$agentPoolProfiles
+    )   
+
+    foreach ($agentProfile in $agentPoolProfiles) {
+        if ($agentProfile.osDiskType -ne "Ephemeral") {
+            return $koMessage
+        }
+    }
+    return $okMessage
+
+}
+
+function CheckDiagnosticSettings {
+    Param(
+        [Parameter(Mandatory = $true)]$cluster
+    )  
+    $diagnosticSettings = az monitor diagnostic-settings list --resource $cluster.id | ConvertFrom-Json
+    if ($diagnosticSettings) { 
+        return $okMessage 
+    } 
+    return $koMessage;
+}
+
+
+function CheckNodepoolsSpecification {    
+    Param(
+        [Parameter(Mandatory = $true)]$agentPoolProfiles
+    )   
+
+    foreach ($agentProfile in $agentPoolProfiles) {
+        if ($agentProfile.Mode -eq "User") {
+            return $okMessage
+        }
+    }
+    return $koMessage;
+}
+
+function CheckAvailabilityZones {
+    Param(
+        [Parameter(Mandatory = $true)]$agentPoolProfiles
+    )   
+    $countNodepool = 0;
+    $countNodePoolWithAZ = 0;
+    foreach ($agentProfile in $agentPoolProfiles) {
+        $countNodepool++;
+        if (($agentProfile.availabilityZones.Count -ge 2) -and ($agentProfile.count -ge 2)) {
+            $countNodePoolWithAZ++ 
+        }
+    }
+    if ($countNodepool -gt $countNodePoolWithAZ) {
+        return $koMessage
+    }
+    else {
+        return $okMessage
+    }
+}
+
+function CheckKubernetesControlPlaneVersion {
+    Param(
+        [Parameter(Mandatory = $true)]$cluster
+    )
+    
+    $aksVersions = $(az aks get-versions --location $cluster.location --output json | ConvertFrom-Json).values.version
+    $result = $okMessage
+    if (-not (($aksVersions | % { $cluster.kubernetesVersion.contains($_) }) -contains $True)) {
+        Write-Verbose "Cluster version $($cluster.kubernetesVersion) is not supported"
+        $result = $koMessage 
+    }
+
+    foreach ($pool in $cluster.agentPoolProfiles) {
+        if (-not (($aksVersions | % { $pool.orchestratorVersion.contains($_) }) -contains $True)) {
+            Write-Verbose "Pool orchestrator version $($pool.orchestratorVersion) is not supported"
+            $result = $koMessage
+        }
+    }
+
+    return $result
+}
+
+function GetTotalNodeCount() {
+    Param(
+        [Parameter(Mandatory = $true)]$cluster
+    )
+
+    $totalNodeCount = 0
+
+    foreach ($nodePool in $cluster.agentPoolProfiles) {
+        $totalNodeCount += $nodePool.count
+    }
+
+    return $totalNodeCount;
+}
+
+function Get-Compliancy {
+    Param(
+        [Parameter(Mandatory = $true)]$clusterResult
+    )   
+    if ($clusterResult.IsKubernetesVersionSupported -ne "" -or
+        $clusterResult.PrivateCluster -eq $koMessage -or
+        $clusterResult.LoadBalancerWithoutPublicIp -eq $koMessage -or
+        $clusterResult.NodePoolSubnetWithNSG -eq $koMessage -or
+        $clusterResult.NetworkPolicyAzureCalico -eq $koMessage -or
+        $clusterResult.ContainerInsights -eq $koMessage -or 
+        $clusterResult.DiagnosticSettings -eq $koMessage -or
+        $clusterResult.UserAssignedIdentity -eq $koMessage -or
+        $clusterResult.PodIdentityDeprecated -eq $koMessage -or
+        $clusterResult.MicrosoftDefender -eq $koMessage -or
+        $clusterResult.EnableRBAC -eq $koMessage -or
+        $clusterResult.KMSConfigured -eq $koMessage -or
+        $clusterResult.SystemAndUserNodePool -eq $koMessage -or
+        $clusterResult.EnableAzurePolicy -eq $koMessage -or
+        $clusterResult.AzureADIntegration -eq $koMessage -or
+        $clusterResult.PrivateCluster -eq $koMessage -or
+        $clusterResult.AvailabilityZones -eq $koMessage -or
+        $clusterResult.UptimeSlaConfiguration -eq $koMessage) {
+        return $koMessage;
+    }
+   
+    return  $okMessage;
+}
+
+function Export-AKSClusterResult {
+    Param(
+        [Parameter(Mandatory = $true)]$clusterResult
+    )   
+    $clusterResult | Export-Csv $exportASKClusterAssessment -NoTypeInformation -Append -Delimiter $csvDelimiter   
+}
+
+#=========================== Main code
+$today = [DateTime]::Now.ToString("yyyy-MM-dd_hh-mm-ss")
+
+
+Write-Host "******** Welcome to the Microsoft Azure Kubernetes Cluster assessment" -ForegroundColor Green
+
+
+if ($Path.Trim() -eq '' -or -not(Test-Path -Path $Path)) {
+    Write-Host "- Please insert the Path where to save the Assessment result" -ForegroundColor Blue
+    $Path = $PSScriptRoot
+    Write-Host "- Default Path: $Path" -ForegroundColor Blue
+}
+
+Start-Transcript -Path "$Path\AKSClusterAssessment_$today\Assessment_Log_$today.txt"
+$exportASKClusterAssessment = "$Path\AKSClusterAssessment_$today\Assessment_Result_$today.csv"
+
+Start-AKSClusterAssessment
+
+Stop-Transcript
